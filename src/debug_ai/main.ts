3a904691e72af64d93d2cf0900a917712b512487
Arthur Le Bourg <lebourgarthur@gmail.com>
2023-07-01 00:36:41 +0200

    better superposition

--- src/debug_ai/main.ts
+++ src/debug_ai/main.ts
@@ -80,6 +80,7 @@ import '@tensorflow/tfjs-backend-webgl';
 
 import {SemanticSegmentation, load} from '@tensorflow-models/deeplab';
 import * as tf from '@tensorflow/tfjs-core';
+import { DeepLabOutput } from '@tensorflow-models/deeplab/dist/types';
 
 let deeplab : SemanticSegmentation;
 
@@ -90,16 +91,62 @@ const initializeModels = async () => {
     await runDeeplab();
 };
 
-const displaySegmentationMap = (deeplabOutput : any) => {
- const {legend, height, width, segmentationMap} = deeplabOutput;
+const displaySegmentationMap = async (deeplabOutput : DeepLabOutput, initialisationStart : any) => {
+ const {height, width, segmentationMap} = deeplabOutput;
  const canvas = document.getElementById('output-image')! as HTMLCanvasElement;
  const ctx = canvas.getContext('2d')!;
 
- const segmentationMapData = new ImageData(segmentationMap, width, height);
+// Assuming you have the RGB values of the classes you want to filter out
+const classColor1: [number, number, number] = [61, 230, 250]; // First class color to filter (e.g., red)
+const classColor2: [number, number, number] = [9, 7, 230]; // Second class color to filter (e.g., green)
+
+// Filter out the two specified classes by comparing RGB values
+const filteredImage = new Uint8ClampedArray(segmentationMap.length);
+
+// get image as array also
+const image = document.getElementById('image')! as HTMLImageElement;
+
+const imageCanvas = document.createElement('canvas');
+imageCanvas.width = width;
+imageCanvas.height = height;
+const imageCtx = imageCanvas.getContext('2d')!;
+imageCtx.drawImage(image, 0, 0, width, height);
+const imageData = imageCtx.getImageData(0, 0, width, height);
+
+const imageArray = imageData.data;
+
+for (let i = 0; i < segmentationMap.length; i += 4) {
+  const r = segmentationMap[i];
+  const g = segmentationMap[i + 1];
+  const b = segmentationMap[i + 2];
+
+  if (
+    (r === classColor1[0] && g === classColor1[1] && b === classColor1[2] ) ||
+      (r === classColor2[0] && g === classColor2[1] && b === classColor2[2])
+    ) {
+      // Copy the RGB values to the filtered image array
+      filteredImage[i] = 61;
+      filteredImage[i + 1] = 230;
+      filteredImage[i + 2] = 250;
+      filteredImage[i + 3] = segmentationMap[i + 3]; // Copy the alpha value
+    }
+    else
+    {
+        // Copy the RGB values to the filtered image array
+      filteredImage[i] = imageArray[i];
+      filteredImage[i + 1] = imageArray[i + 1];
+      filteredImage[i + 2] = imageArray[i + 2];
+      filteredImage[i + 3] = imageArray[i + 3]; // Copy the alpha value
+    }
+}
+
+const segmentationMapData = new ImageData(filteredImage, width, height);
+ //const segmentationMapData = new ImageData(segmentationMap, width, height);
  canvas.style.width = '100%';
  canvas.style.height = '100%';
  canvas.width = width;
  canvas.height = height;
+
  ctx.putImageData(segmentationMapData, 0, 0);
 
  const legendList = document.getElementById('legend')!;
@@ -107,18 +154,8 @@ const displaySegmentationMap = (deeplabOutput : any) => {
    legendList.removeChild(legendList.firstChild);
  }
 
- Object.keys(legend).forEach((label) => {
-   const tag = document.createElement('span');
-   tag.innerHTML = label;
-   const [red, green, blue] = legend[label];
-   tag.classList.add('column');
-   tag.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-   tag.style.padding = '1em';
-   tag.style.margin = '1em';
-   tag.style.color = '#ffffff';
-
-   legendList.appendChild(tag);
- });
+ status(`Ran in ${
+    ((performance.now() - initialisationStart) / 1000).toFixed(2)} s`);
 
 };
 
@@ -130,9 +167,7 @@ const status = (message : string) => {
 
 const runPrediction = (input : any, initialisationStart : any) => {
    deeplab.segment(input).then((output) => {
-     displaySegmentationMap(output);
-     status(`Ran in ${
-         ((performance.now() - initialisationStart) / 1000).toFixed(2)} s`);
+     displaySegmentationMap(output, initialisationStart);
    });
 };
 
