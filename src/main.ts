330d2a3f2d415b559f170446cb3e79578205827d
Arthur Le Bourg <lebourgarthur@gmail.com>
2023-09-27 17:32:55 +0200

    trying to get result as image for debug purposes

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,46 @@ import { ARButton } from 'three/examples/jsm/webxr/ARButton.js'
 import { MultiplayerScene } from './multiplayer_scene'
 import { CameraModule } from './camera_module'
 
+function combineImages(baseImageData: ImageData, overlayImageData: ImageData | undefined): ImageData {
+    if (!overlayImageData) {
+        return baseImageData;
+    }
+
+    const combined_image = new Uint8ClampedArray(baseImageData.data.length);
+
+    for (let i = 0; i < overlayImageData.data.length; i += 4) {
+      // Get the RGB values of the overlay image
+      const overlayR = overlayImageData.data[i];
+      const overlayG = overlayImageData.data[i + 1];
+      const overlayB = overlayImageData.data[i + 2];
+      const overlayA = overlayImageData.data[i + 3];
+
+      if (overlayA === 0) {
+        // Set the result pixel values
+        combined_image[i] = overlayR;
+        combined_image[i + 1] = overlayG;
+        combined_image[i + 2] = overlayB;
+        combined_image[i + 3] = overlayA;
+      }
+      else
+      {
+        combined_image[i] = baseImageData.data[i];
+        combined_image[i + 1] = baseImageData.data[i + 1];
+        combined_image[i + 2] = baseImageData.data[i + 2];
+        combined_image[i + 3] = baseImageData.data[i + 3];
+
+      }
+    }
+    
+    return new ImageData(combined_image, baseImageData.width, baseImageData.height);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const renderer = new WebGLRenderer({ preserveDrawingBuffer: true, antialias: true })
     renderer.xr.enabled = true
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.domElement.style.border = '10px solid black';
+    renderer.domElement.style.border = '10px solid blue';
     document.body.appendChild(renderer.domElement);
 
     const scene = new MultiplayerScene();
@@ -35,13 +69,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const offscreen_canvas = new OffscreenCanvas(0, 0);
     const offscreen_canvas_context = offscreen_canvas.getContext('2d', { willReadFrequently: true })!;
 
+    let inference_result: ImageData;
+
     inference_worker.onmessage = (event) => {
         const { data } = event;
         take_picture = true;
         const imageData = data.segmentationMapData;
         inference_result_as_texture.image = imageData;
         inference_result_as_texture.needsUpdate = true;
+        inference_result = imageData;
     }
+
+    const debug_canvas = document.createElement('canvas');
+    debug_canvas.style.border = '10px solid red';
+    let debug_context = debug_canvas.getContext('2d')!;
+    document.body.appendChild(debug_canvas);
 
     button.addEventListener('click', () => {
         inference_worker.postMessage({ type: "start" });
@@ -57,7 +99,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         const render_target = renderer.getRenderTarget();
                         renderer.setRenderTarget(null);
                         renderer.render(empty_scene, camera);
-                        renderer.setRenderTarget(render_target);
                         take_picture = false;
 
                         offscreen_canvas.width = size[0];
@@ -66,7 +107,21 @@ document.addEventListener('DOMContentLoaded', () => {
                         offscreen_canvas_context.drawImage(renderer.domElement, 0, 0, size[0], size[1]);
 
                         const image_data = offscreen_canvas_context.getImageData(0, 0, size[0], size[1]);
+
+                        // display the balanced image for debugging with an alert
+
+                        if (inference_result) {
+                            // combine the inference result with the camera image
+                            const result_on_image = combineImages(image_data, inference_result);
+
+                            debug_canvas.width = size[0];
+                            debug_canvas.height = size[1];
+                            debug_context = debug_canvas.getContext('2d')!;
+                            debug_context.putImageData(result_on_image, 0, 0);
+                        }
+
                         inference_worker.postMessage({ image: image_data });
+                        renderer.setRenderTarget(render_target);
                     }
                 }
                 renderer.render(scene, camera)
